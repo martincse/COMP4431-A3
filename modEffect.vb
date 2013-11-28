011f7d26fdc3dcd390951de7f2573f13b4d5e6c6
martincse <kychanas@stu.ust.hk>
2013-11-28 18:27:25 +0800

    modify modeffect.vb

--- modEffect.vb
+++ modEffect.vb
@@ -2,6 +2,25 @@ Module modEffect
 
     Public Function IsKeyframe(ByRef input1 As Bitmap, ByRef input2 As Bitmap, _
                                ByVal threshold As Double, ByVal similarity As Double) As Boolean
+        Dim diff As Integer = 0
+        Dim total As Integer = 0
+        For x As Integer = input1.Width / 4 To input1.Width * 3 / 4
+            For y As Integer = input1.Height / 4 To input1.Height * 3 / 4
+                total = total + 1
+                Dim c1 As Color = input1.GetPixel(x, y)
+                Dim c2 As Color = input2.GetPixel(x, y)
+                Dim red As Integer = Math.Abs(CInt(c1.R) - CInt(c2.R))
+                Dim green As Integer = Math.Abs(CInt(c1.G) - CInt(c2.G))
+                Dim blue As Integer = Math.Abs(CInt(c1.B) - CInt(c2.B))
+                Dim colorDiff As Double = CDbl(red + green + blue) / CDbl(255 * 3)
+                If colorDiff > threshold Then
+                    diff = diff + 1
+                End If
+            Next
+        Next
+        If (CDbl(diff) / CDbl(total) > similarity) Then
+            Return True
+        End If
 
         Return False
     End Function
@@ -49,11 +68,45 @@ Module modEffect
         Return output
     End Function
 
+    Dim frames As Generic.List(Of Bitmap) = New Generic.List(Of Bitmap)
+
     Public Function MotionBlur(ByRef input As Bitmap, ByVal blurCount As Integer, _
                                ByVal currentIndex As Integer, ByVal startFrame As Integer, ByVal endFrame As Integer, ByVal newOp As Boolean, _
                                ByVal dirname As String) As Bitmap
         Dim output As New Bitmap(input.Width, input.Height, Imaging.PixelFormat.Format24bppRgb)
-        ' You need to initialize the buffers at the start of the operation
+
+        Dim r, g, b As Integer
+        Dim c As Color
+        Dim buffer As Bitmap = New Bitmap(input.Width, input.Height, Imaging.PixelFormat.Format24bppRgb)
+
+        If (frames.Count = blurCount) Then
+
+            For frame As Integer = currentIndex - blurCount + 1 To currentIndex
+                For x As Integer = 0 To output.Width - 1
+                    For y As Integer = 0 To output.Height - 1
+                        c = GetPixel(input, x, y)
+                        r = c.R + GetPixel(frames.Item(frame), x, y).R
+                        g = c.G + GetPixel(frames.Item(frame), x, y).G
+                        b = c.B + GetPixel(frames.Item(frame), x, y).B
+                        buffer.SetPixel(x, y, Color.FromArgb(r, g, b))
+                    Next
+                Next
+            Next
+
+            For x As Integer = 0 To output.Width - 1
+                For y As Integer = 0 To output.Height - 1
+                    c = GetPixel(buffer, x, y)
+                    r = c.R / blurCount
+                    g = c.G / blurCount
+                    b = c.B / blurCount
+                    output.SetPixel(x, y, Color.FromArgb(r, g, b))
+                Next
+            Next
+
+            frames.Clear()
+        Else
+            frames.Add(input)
+        End If
 
         Return output
     End Function
@@ -93,6 +146,41 @@ Module modEffect
                          ByVal currentIndex As Integer, ByVal startFrame As Integer, ByVal endFrame As Integer, ByVal newOp As Boolean) As Bitmap
         Dim output As New Bitmap(input.Width, input.Height, Imaging.PixelFormat.Format24bppRgb)
         Static Dim displacement() As Integer = Nothing
+
+        If newOp Then
+            ReDim displacement(input.Width - 1)
+            If useSine Then
+                For x As Integer = 0 To input.Width - 1
+                    displacement(x) = displacement(x) + Math.Abs(amplitude * Math.Sin(2.0# * Math.PI * cycle * x / (input.Width - 1)))
+                Next
+            End If
+            If useRandom Then
+                Dim currentOffset As Integer
+                Dim currentPeriod As Integer
+                Dim sign As Integer
+                Dim x As Integer = 0
+
+                While x < input.Width
+                    currentPeriod = 1 + Rnd() * (period - 1)
+                    If (Rnd() >= 0.5) Then
+                        sign = 1
+                    Else
+                        sign = -1
+                    End If
+                    For i As Integer = x To x + currentPeriod - 1
+                        currentOffset = currentOffset + sign * Rnd() * offset
+                        If currentOffset < 0 Then
+                            currentOffset = -1 * currentOffset
+                            sign = -1 * sign
+                        End If
+                        If i < input.Width Then
+                            displacement(i) = displacement(i) + currentOffset
+                        End If
+                    Next
+                    x = x + currentPeriod
+                End While
+            End If
+        End If
 
         Return output
     End Function
